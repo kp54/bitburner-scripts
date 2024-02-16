da1bcf9b11aed89efbd8eeb38dd21e4742ecb32c
kp54 <kangpang65@gmail.com>
2024-02-17 03:48:48 +0900

    find multiple targets

--- src/find-optim-target.ts
+++ src/find-optim-target.ts
@@ -3,10 +3,14 @@ import { canHack, openNuke } from "lib/hack-kit";
 import { walk } from "lib/net-walker";
 
 export const main = async (ns: NS) => {
-  let current_max = 0;
-  let current_host = "(none)";
+  let limit = 5;
+  if (ns.args.length === 1) {
+    limit = Number(ns.args[0]);
+  }
 
-  const work = (ns: NS, host: string) => {
+  const hosts = new Array<{ host: string; score: number }>();
+
+  await walk(ns, (ns, host) => {
     if (!canHack(ns, host) || !openNuke(ns, host)) {
       return;
     }
@@ -19,15 +23,17 @@ export const main = async (ns: NS) => {
       return;
     }
 
-    const maxMon = ns.getServerMaxMoney(host);
+    const score = ns.getServerMaxMoney(host);
+    hosts.push({ host, score });
+  });
 
-    if (current_max < maxMon) {
-      current_max = maxMon;
-      current_host = host;
-    }
-  };
+  hosts.sort((x, y) => y.score - x.score);
+  if (limit !== -1) {
+    hosts.splice(5);
+  }
 
-  await walk(ns, work);
-
-  ns.tprint(`Host: ${current_host}, MaxMon: ${current_max}`);
+  const lines = hosts
+    .map((x) => `${x.host}: ${ns.formatNumber(x.score)}`)
+    .join("\n");
+  ns.tprint(`\n${lines}`);
 };
