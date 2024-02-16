6358d67c672697f07de9ad7c38447da340e1db68
kp54 <kangpang65@gmail.com>
2024-02-17 02:34:01 +0900

    setup workspace/biome

--- src/lib/react.ts
+++ src/lib/react.ts
@@ -1,10 +1,8 @@
-import ReactNamespace from 'react/index';
-import ReactDomNamespace from 'react-dom';
+import ReactDomNamespace from "react-dom";
+import ReactNamespace from "react/index";
 
 const React = window.React as typeof ReactNamespace;
 const ReactDOM = window.ReactDOM as typeof ReactDomNamespace;
 
 export default React;
-export {
-  ReactDOM
-}
+export { ReactDOM };
