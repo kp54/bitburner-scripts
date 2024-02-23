c8f990d08cca2f5dc8ce8d1fa8a7447eb8ff7a2a
kp54 <kangpang65@gmail.com>
2024-02-23 14:08:03 +0900

    use memoized recursion

--- src/cct/total-ways-to-sum.ts
+++ src/cct/total-ways-to-sum.ts
@@ -14,29 +14,37 @@ How many different distinct ways can the number 51 be written as a sum of at lea
 
 import { NS } from "@ns";
 
-const rec = (num: number, choices: number[]) => {
+const rec = (num: number, choices: number[], memo: Map<string, number>) => {
   const x: number | undefined = choices[0];
   if (x === undefined || num < x) {
     return 0;
+  }
+
+  const id = `${num}-${x}`;
+  const cache = memo.get(id);
+  if (cache !== undefined) {
+    return cache;
   }
 
   let sum = 0;
   let i = 0;
   for (; i < num; i += x) {
     const next = choices.slice(1);
-    sum += rec(num - i, next);
+    sum += rec(num - i, next, memo);
   }
 
   if (i === num) {
     sum += 1;
   }
 
+  memo.set(id, sum);
   return sum;
 };
 
 export const totalWaysToSum = (num: number) => {
   const choices = new Array(num - 1).fill(0).map((_, i) => i + 1);
-  return rec(num, choices);
+  const memo = new Map<string, number>();
+  return rec(num, choices, memo);
 };
 
 export const main = (ns: NS) => {
