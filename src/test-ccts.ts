175ee859b34b2cc2f9963fc7cf4555e14d6e71e2
kp54 <kangpang65@gmail.com>
2024-02-23 15:36:27 +0900

    tweak cct tester

--- src/test-ccts.ts
+++ src/test-ccts.ts
@@ -1,7 +1,53 @@
 import { NS } from "@ns";
 import { solvers } from "./cct/index";
 
+const test = async (ns: NS, host: string, type: string) => {
+  const solver = solvers.get(type);
+  if (solver === undefined) {
+    return true;
+  }
+
+  ns.tprint(`testing implementation for '${type}'...`);
+
+  const startAt = Date.now();
+  for (let i = 0; i < 128; i++) {
+    ns.codingcontract.createDummyContract(type);
+    const file = ns
+      .ls(host)
+      .filter((x) => x.endsWith(".cct"))
+      .shift();
+    if (file === undefined) {
+      throw new Error();
+    }
+
+    const input = ns.codingcontract.getData(file, host);
+    const output = solver(input);
+    const result = ns.codingcontract.attempt(output, file, host);
+    if (result === "") {
+      ns.tprint("- failed.");
+      return false;
+    }
+
+    ns.rm(file, host);
+    await ns.asleep(10);
+  }
+
+  const elapsed = Date.now() - startAt;
+  ns.tprint(`- succeeded in ${elapsed} ms.`);
+  return true;
+};
+
 export const main = async (ns: NS) => {
+  if (1 < ns.args.length) {
+    ns.tprint("usage: test-ccts [TYPE]");
+    return;
+  }
+
+  let type: string | null = null;
+  if (ns.args.length === 1) {
+    type = String(ns.args[0]);
+  }
+
   const host = ns.getHostname();
 
   for (const file of ns.ls(host)) {
@@ -10,39 +56,12 @@ export const main = async (ns: NS) => {
     }
   }
 
-  const types = ns.codingcontract.getContractTypes();
-  for (const type of types) {
-    const solver = solvers.get(type);
-    if (solver === undefined) {
-      continue;
+  if (type !== null) {
+    await test(ns, host, type);
+  } else {
+    const types = ns.codingcontract.getContractTypes();
+    for (const type of types) {
+      await test(ns, host, type);
     }
-
-    ns.tprint(`testing implementation for '${type}'...`);
-
-    const startAt = Date.now();
-    for (let i = 0; i < 128; i++) {
-      ns.codingcontract.createDummyContract(type);
-      const file = ns
-        .ls(host)
-        .filter((x) => x.endsWith(".cct"))
-        .shift();
-      if (file === undefined) {
-        throw new Error();
-      }
-
-      const input = ns.codingcontract.getData(file, host);
-      const output = solver(input);
-      const result = ns.codingcontract.attempt(output, file, host);
-      if (result === "") {
-        ns.tprint("- failed.");
-        return;
-      }
-
-      ns.rm(file, host);
-      await ns.asleep(10);
-    }
-
-    const elapsed = Date.now() - startAt;
-    ns.tprint(`- succeeded in ${elapsed} ms.`);
   }
 };
