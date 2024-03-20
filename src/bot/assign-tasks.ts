73c4f468accb22210f02a15db039f9829974bcfe
kp54 <kangpang65@gmail.com>
2024-03-21 02:40:54 +0900

    tweak task assigning

--- src/bot/assign-tasks.ts
+++ src/bot/assign-tasks.ts
@@ -3,23 +3,38 @@ import { GangTasks } from "/const/gang-tasks";
 
 const trainThres = 100;
 
+const tasks = [
+	GangTasks.Ransomware,
+	GangTasks.Phishing,
+	GangTasks.IdentityTheft,
+	GangTasks.DDoSAttacks,
+	GangTasks.PlantVirus,
+	GangTasks.FraudAndCounterfeiting,
+	GangTasks.MoneyLaundering,
+	GangTasks.Cyberterrorism,
+];
+
 export const assignTasks = async (ns: NS) => {
 	const members = ns.gang.getMemberNames();
 	for (const member of members) {
 		const mem = ns.gang.getMemberInformation(member);
 		if (mem.hack < trainThres) {
 			ns.gang.setMemberTask(member, GangTasks.TrainHacking);
-			await ns.gang.nextUpdate();
 			continue;
 		}
 
-		ns.gang.setMemberTask(member, GangTasks.IdentityTheft);
-		await ns.gang.nextUpdate();
+		let prevTask: string = GangTasks.EthicalHacking;
+		for (const task of tasks) {
+			ns.gang.setMemberTask(member, task);
 
-		const info = ns.gang.getGangInformation();
-		if (0 < info.wantedLevelGainRate) {
-			ns.gang.setMemberTask(member, GangTasks.EthicalHacking);
 			await ns.gang.nextUpdate();
+			const info = ns.gang.getGangInformation();
+			if (0 < info.wantedLevelGainRate) {
+				ns.gang.setMemberTask(member, prevTask);
+				break;
+			}
+
+			prevTask = task;
 		}
 	}
 };
